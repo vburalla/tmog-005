ef3e10934a0a80225696ae2529df786fc6e01399
Víctor Buralla Blasco <vburalla@mercadona.es>
2024-02-24 16:48:37 +0100

    first version

--- src/main/java/org/tfoc/MinStack.java
+++ src/main/java/org/tfoc/MinStack.java
@@ -1,5 +1,7 @@
 package org.tfoc;
 
+import java.util.*;
+
 /**
  * MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack()
@@ -10,26 +12,46 @@ package org.tfoc;
  */
 public class MinStack {
 
-    public MinStack() {
+    Stack<Integer> stack;
+    List<Integer> sortedStack;
+    int min = Integer.MAX_VALUE;
 
+    public MinStack() {
+        this.stack = new Stack<>();
+        this.sortedStack = new ArrayList<>();
     }
 
     public void push(int val) {
-
+        this.stack.push(val);
+        this.sortedStack.add(val);
+        Collections.sort(sortedStack);
+        if (val < min) {
+            min = val;
+        }
     }
 
-    public void pop() {
-
+    public void pop() throws Exception {
+        if (this.sortedStack.isEmpty())
+            throw new Exception("Cannot remove elements from empty MinStack");
+        else {
+            Integer value = this.stack.pop();
+            this.sortedStack.remove(value);
+            if (value.equals(this.min))
+                min = sortedStack.isEmpty() ? Integer.MAX_VALUE : sortedStack.get(0);
+        }
     }
 
-    public int top() {
-
-        return 0;
+    public int top() throws Exception {
+        if (this.stack.isEmpty())
+            throw new Exception("MinStack is empty");
+        return stack.peek();
     }
 
-    public int getMin() {
+    public int getMin() throws Exception {
 
-        return 0;
+        if (this.sortedStack.isEmpty())
+            throw new Exception("MinStack is empty");
+        return this.min;
     }
 
 }
--- src/test/java/org/tfoc/MinStackTest.java
+++ src/test/java/org/tfoc/MinStackTest.java
@@ -1,23 +1,51 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 class MinStackTest {
 
-    @Test
-    void testMinStack() {
+    MinStack minStack;
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    @BeforeEach
+    void setUp() {
+        this.minStack = new MinStack();
+    }
+    @Test
+    void testMinStack() throws Exception {
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        assertEquals(-3, minStack.getMin());
+        minStack.pop();
+        assertEquals(0,minStack.top());
+        assertEquals(-2, minStack.getMin());
+
     }
 
     @Test
-    void push() {
+    void push() throws Exception {
+
+        minStack.push(10);
+        assertEquals(10, minStack.top());
+        assertEquals(10, minStack.getMin());
     }
 
     @Test
-    void pop() {
+    void testPopThrows() {
+
+        boolean thrown = false;
+        try {
+            minStack.pop();
+        } catch (Exception ex) {
+            thrown = true;
+        }
+        assertTrue(thrown);
     }
 
     @Test
